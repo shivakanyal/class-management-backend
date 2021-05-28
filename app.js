f08a1e4ec50a9f806c298047740bfc1582f40c11
Shiva Kanyal <kanyalshiva1@gmail.com>
2021-05-28 10:52:08 +0530

    initial commit .

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.listen(PORT, (err) => {
+  err ? console.log(err) : console.log("port is running on port " + PORT);
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Express </h1>");
+});
